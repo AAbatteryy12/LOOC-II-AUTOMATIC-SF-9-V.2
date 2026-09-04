1de0dbf9c7cc505bcb4ea795746115aec3bfa681
AAbatteryy12 <aarongojardelossantos@gmail.com>
2026-09-04 22:10:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Put your Supabase Project URL and Publishable Key here.
 window.SF9_CONFIG = {
-  url: "https://aqbfkazeikwvsoozxjzy.supabase.co",
+  url: "https://aqbfkazeikwvsoozxjzy.supabase.com",
   publishableKey: "sb_publishable_B_th7aCzzvzOAC1s0FwZQQ_p73M1Jzp"
 };
